b987220647de691af702598159030273a1ecdcce
YTKore <rikytejerina@gmail.com>
2018-10-27 00:13:38 -0400

    Prueba 2

---
+++ UVA.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n,p;
+  string s;
+  scanf("%d %d", &n, &p);
+  cin.ignore();
+  for(int i=0;i<n;i++)
+  {
+    getline(cin,s);
+    //cout<<s<<endl;
+  }
+  printf("%d\n",p);
+}
